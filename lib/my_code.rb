4ecd63b93fca502885ec9872731c36f6dc21938b
Enrique Pittaluga <ep7m@me.com>
2020-03-10 22:49:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,7 +58,7 @@ def reduce_to_total(source_array, starting_point)
   i = starting_point
   total = 0
   while (i < source_array.length) do 
-    
+    total += source_array[i]
     i += 1
   end 
   
