706d5fc8ab4fb27f9b0ff5b39c9735f572c9458d
Enrique Pittaluga <ep7m@me.com>
2020-03-10 22:51:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -71,7 +71,9 @@ def reduce_to_all_true(source_array)
   i = 0 
   
   while (i < source_array.length) do 
-    
+    if (source_array[i] == true)
+      return true
+    end
     i += 1
   end 
   
