5eb3e718310cda2f978e5b012a27b52b4e4063af
Enrique Pittaluga <ep7m@me.com>
2020-03-10 22:57:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,17 +55,17 @@ end
 
 
 def reduce_to_total(source_array, starting_point=0)
-
+   
     i = starting_point
-  
-  total = 0
-  while (i < source_array.length) do 
-    total += source_array[i]
-    i += 1
-  end 
-  
-  return total
-  
+    total = 0
+
+    while (i < source_array.length) do 
+      total += source_array[i]
+      i += 1
+    end 
+    
+    return total
+    
 end
 
 def reduce_to_all_true(source_array)
