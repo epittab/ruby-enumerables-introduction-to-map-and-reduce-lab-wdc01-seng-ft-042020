11c56b719b186630a409878e82a0788aea5fdb7e
Enrique Pittaluga <ep7m@me.com>
2020-03-10 22:45:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,14 +14,38 @@ def map_to_negativize(source_array)
 end 
 
 def map_to_no_change(source_array)
+  i = 0 
+  map_array = []
+  while (i < source_array.length) do 
+    
+    i += 1
+  end 
+  
+  return map_array
   
 end 
 
 def map_to_double(source_array)
+  i = 0 
+  map_array = []
+  while (i < source_array.length) do 
+    
+    i += 1
+  end 
+  
+  return map_array
   
 end 
 
 def map_to_square(source_array)
+  i = 0 
+  map_array = []
+  while (i < source_array.length) do 
+    
+    i += 1
+  end 
+  
+  return map_array
   
   
 end 
@@ -32,7 +56,14 @@ end
 
 
 def reduce_to_total(source_array, starting_point)
+  i = 0 
+  total = 0
+  while (i < source_array.length) do 
+    
+    i += 1
+  end 
   
+  return total
   
   
   
