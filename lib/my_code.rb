12f978c7e01e410568cd83d523842d3aeee49555
Enrique Pittaluga <ep7m@me.com>
2020-03-10 22:43:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,24 @@
 # My Code here....
 
-def map_
+# all map methods return a new Array
+
+def map_to_negativize(source_array)
+  
+  
+end 
+
+def map_to_no_change(source_array)
+  
+end 
+
+def map_to_double(source_array)
+  
+end 
+
+def map_to_square(source_array)
+  
+  
+end 
+
+
+# all reduce methods return a value 
